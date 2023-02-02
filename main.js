6836a6dc439e52585f59379c0fc40fb5d044f51c
ometman <114116376+ometman@users.noreply.github.com>
2023-02-02 15:05:12 +0000

    Update local storage code for form input

--- main.js
+++ main.js
@@ -189,25 +189,25 @@ form.addEventListener('submit', (e) => {
   }
 });
 
-// store data in browser
+//store data in browser
 
-// const username = document.querySelector('#name');
-// const useremail = document.querySelector('#email');
-// const text = document.querySelector('#textarea');
+const username = document.querySelector('#name');
+const useremail = document.querySelector('#email');
+const text = document.querySelector('#textarea');
 
-// form.addEventListener('input', () => {
+form.addEventListener('input', () => {
 
-//   const formdata = {
-//     username: username.value,
-//     useremail: useremail.value,
-//     text: text.value
-//   }
+  const formdata = {
+    username: username.value,
+    useremail: useremail.value,
+    text: text.value
+  }
   
-//   localStorage.setItem('data', JSON.stringify(formdata));
-// });
+  localStorage.setItem('data', JSON.stringify(formdata));
+});
 
-// const formdataparsed = JSON.parse(localStorage.getItem('data'));
+const formdataparsed = JSON.parse(localStorage.getItem('data'));
 
-// username.value =formdataparsed.username;
-// useremail.value =formdataparsed.useremail;
-// text.value =formdataparsed.text;
+username.value =formdataparsed.username;
+useremail.value =formdataparsed.useremail;
+text.value =formdataparsed.text;
